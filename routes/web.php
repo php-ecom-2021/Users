aedc9d1175d25eb57e8bddb9e67348399b33a101
ZombieLicker <jakob.tp@hotmail.com>
2021-11-30 14:08:59 +0100

    added stuff

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('product', function(){
+    $products = \App\Models\Product::all();
+    return view('product', ['products' => $products]);
+});
